5e59726fb4ed1dd554446e9c5ffb0fa9217a2437
robert.lupoaie <robert.lupoaie@ucms.ro>
2025-02-19 00:07:00 +0200

    3.3-3.4 Cross-Cutting Concerns Module + Logg with errors

--- Evently.Api/Program.cs
+++ Evently.Api/Program.cs
@@ -2,8 +2,11 @@ using Evently.Api.Extensions;
 using Evently.Common.Application;
 using Evently.Common.Infrastructure;
 using Evently.Modules.Events.Infrastructure;
+using Serilog;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, LoggerConfig) => LoggerConfig.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen( options =>
@@ -14,6 +17,8 @@ builder.Services.AddSwaggerGen( options =>
 builder.Services.AddApplication([Evently.Modules.Events.Application.AssemblyReference.Assembly]);
 
 builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("Database")!);
+
+builder.Configuration.AddModuleConfiguration(["events"]);
 
 builder.Services.AddEventsModule(builder.Configuration);
 
@@ -28,5 +33,7 @@ if (app.Environment.IsDevelopment())
 }
 
 EventsModule.MapEndpoints(app);
+
+app.UseSerilogRequestLogging();
 
 app.Run();
