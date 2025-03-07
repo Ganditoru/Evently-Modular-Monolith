294c803f993e0402fa0e829310468f073ba44029
robert.lupoaie <robert.lupoaie@ucms.ro>
2025-03-07 11:20:01 +0200

    4.8 New O

--- src/Modules/Ticketing/Evently.Modules.Ticketing.Domain/Events/TicketTypeErrors.cs
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Domain/Events/TicketTypeErrors.cs
@@ -5,4 +5,9 @@ public static class TicketTypeErrors
 {
     public static Error NotFound(Guid ticketTypeId) =>
         Error.NotFound("TicketTypes.NotFound", $"The ticket type with the identifier {ticketTypeId} was not found");
+
+    public static Error NotEnoughQuantity(decimal availableQuantity) =>
+        Error.Problem(
+            "TicketTypes.NotEnoughQuantity",
+            $"The ticket type has {availableQuantity} quantity available");
 }
